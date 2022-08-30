b94a1ee49e297e8c0537ffb4852ea369945371b6
윤성 <yunyul1209@gmail.com>
2022-08-30 18:50:58 +0900

    feat : 현재 씬(scroll-section)에서 스크롤된 범위를 비율로 구하기

--- js/index.js
+++ js/index.js
@@ -11,6 +11,13 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -46,9 +53,7 @@
     //각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[
-        i
-      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
 
     // 페이지 로드 시 현재 스크롤 위치에 맞게 currentScene 세팅
@@ -61,6 +66,36 @@
       }
     }
     document.body.setAttribute('id', `show-scene-${currentScene}`);
+  }
+
+  function calcValues(values, currentScrollY) {
+    // 현재 씬(scroll-section)에서 스크롤된 범위를 비율로 구하기
+    let rv;
+    let scrollRatio = currentScrollY / sceneInfo[currentScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0] + values[0]);
+
+    return rv;
+  }
+
+  function playAnimation() {
+    // 스크롤 애니메이션 진행
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentScrollY = scrollY - prevScrollHeight;
+
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(values.messageA_opacity, currentScrollY);
+        objs.messageA.style.opacity = messageA_opacity_in;
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
   }
 
   function scrollLoop() {
@@ -79,6 +114,7 @@
       currentScene--;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
+    playAnimation();
   }
 
   window.addEventListener('scroll', () => {
