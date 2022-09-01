27c31df4c080e2fcc9782afa2b775614faebc82d
윤성 <yunyul1209@gmail.com>
2022-09-01 20:19:23 +0900

    feat : 특정 타이밍 스크롤 애니메이션 기능 추가(translate)

--- js/index.js
+++ js/index.js
@@ -20,8 +20,9 @@
       values: {
         // 텍스트 opacity 값과 구간 지정
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
-        messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
         messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
       },
     },
     {
@@ -116,13 +117,17 @@
       case 0:
         const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
         const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
+        const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
+        const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
 
         if (scrollRatio <= 0.22) {
           // in
           objs.messageA.style.opacity = messageA_opacity_in;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
         } else {
           // out
           objs.messageA.style.opacity = messageA_opacity_out;
+          objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
         }
         break;
       case 1:
