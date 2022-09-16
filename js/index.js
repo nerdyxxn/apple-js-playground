095d01be840726d515c5fc6358da668504aa5f4c
윤성 <yunyul1209@gmail.com>
2022-09-16 18:52:22 +0900

    feat : section-3 캔버스 이미지 블렌드 처리

--- js/index.js
+++ js/index.js
@@ -110,6 +110,7 @@
       values: {
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
+        blendHeight: [0, 0, { start: 0, end: 0 }],
         rectStartY: 0,
       },
     },
@@ -477,6 +478,27 @@
           objs.canvas.style.top = `${
             -(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2
           }px`;
+
+          // 이미지 블렌드
+          // blendHeight: [0, 0, { start: 0, end: 0 }],
+          values.blendHeight[0] = 0;
+          values.blendHeight[1] = objs.canvas.height;
+          values.blendHeight[2].start = values.rect1X[2].end;
+          // 현재 씬의 전체 scrollHeight가 1이니까, 그 구간에 한 20%정도 스크롤 할 동안 이미지 블렌드가 재생된다고 가정하면
+          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+          objs.context.drawImage(
+            objs.images[1],
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight,
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight
+          );
         }
 
         break;
