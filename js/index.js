94a4df0c397a60a6380160f40f990d3ddbbb6310
윤성 <yunyul1209@gmail.com>
2022-10-05 23:20:26 +0900

    fix :이미지 블렌드 영역에서 리사이즈 했을 때 이미지 깨지는 오류 해결

--- js/index.js
+++ js/index.js
@@ -648,13 +648,16 @@
 
     window.addEventListener('resize', () => {
       if (window.innerWidth > 900) {
-        setLayout();
-        sceneInfo[3].values.rectStartY = 0;
+        window.location.reload();
       }
     });
 
     window.addEventListener('orientationchange', () => {
-      setTimeout(setLayout, 500);
+      // 모바일은 기기마다 경우의 수 다양하기 때문에 -> 스크롤 위로 올려주기
+      scrollTo(0, 0);
+      setTimeout(() => {
+        window.location.reload();
+      }, 500);
     });
 
     document.querySelector('.loading').addEventListener('transitionend', (e) => {
